1351b9f9baeda6de5289cb2609dccd3ee2efb70c
saad <zu8150531@iespoblenou.org>
2024-04-12 22:51:17 +0200

    First commit

--- src/Ex1.java
+++ src/Ex1.java
@@ -1,13 +1,31 @@
 import java.io.File;
+import java.nio.file.FileSystems;
 import java.util.Scanner;
 
 public class Ex1 {
+    public static String getAutomaticPath (String path){
+        String separador = FileSystems.getDefault().getSeparator();
+        String sistemaOperativo = System.getProperty("os.name");
+
+        System.out.println(separador);
+
+        if (sistemaOperativo.toLowerCase().contains("linux")) {
+            System.out.println(path);
+            path = path.replace("\\", separador);
+            System.out.println(path);
+            path = path.replace("C:", "");
+            System.out.println(path);
+        } else if (sistemaOperativo.toLowerCase().contains("windows")) {
+            path = path.replace("/", separador);
+        }
+        return path;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         try {
             File[] directoryList;
             System.out.print("Introdueix el directori: ");
-            File directory = new File(sc.next());
+            File directory = new File(getAutomaticPath(sc.next()));
 
             directoryList = directory.listFiles();
 
--- src/ex6/Ex6.java
+++ src/ex6/Ex6.java
@@ -1,29 +1,42 @@
 package ex6;
 
+
 import java.io.File;
 import java.nio.file.FileSystems;
 
+
 public class Ex6 {
-    public static void main(String[] args) {
-        String path = "src\\ex6\\arxius";
+
+    public static String getAutomaticPath (String path){
         String separador = FileSystems.getDefault().getSeparator();
         String sistemaOperativo = System.getProperty("os.name");
 
+        System.out.println(separador);
+
         if (sistemaOperativo.toLowerCase().contains("linux")) {
-            path = path.replace("/", separador);
-        } else if (sistemaOperativo.toLowerCase().contains("windows")) {
             path = path.replace("\\", separador);
+        } else if (sistemaOperativo.toLowerCase().contains("windows")) {
+            path = path.replace("/", separador);
         }
+        return path;
+    }
+    public static void main(String[] args) {
+        String path = "src\\ex6\\arxius";
+
+        path = getAutomaticPath(path);
+        String separador = FileSystems.getDefault().getSeparator();
 
         System.out.println(path);
 
         try {
+            File directoryArxiuNou = new File(path + separador + "arxiusNou");
             File diretory = new File(path);
             File[] directoryFiles = diretory.listFiles();
+            directoryArxiuNou.mkdir();
             for (File file : directoryFiles) {
                 if (file.isFile()) {
                     if (file.getName().endsWith(".txt")) {
-                        File newDirectory = new File(path + "\\" +
+                        File newDirectory = new File(directoryArxiuNou + separador +
                                 file.getName().substring(0, file.getName().indexOf(".")));
                         System.out.println(newDirectory);
                         System.out.println(newDirectory.mkdir());
@@ -31,7 +44,7 @@ public class Ex6 {
                 }
                 if (file.isDirectory()) {
                     System.out.println(file.toPath());
-                    File newDirectories = new File(path + "\\" + file.getName() + ".txt");
+                    File newDirectories = new File(directoryArxiuNou + separador + file.getName() + ".txt");
                     System.out.println(newDirectories);
                     System.out.println(newDirectories.createNewFile());
                 }
